5c665c9a8ec409fecd185f8f083d6a0677f7e9c4
Mehdi Masmar <masmar507@gmail.com>
2023-09-25 10:43:48 +0100

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let new_array = [];
+    for (let idx of array) {
+        let value = idx;
+        idx = appendString + value;
+        new_array.push(idx);
+    }
+
+    return new_array;
+}
