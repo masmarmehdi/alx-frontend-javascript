c6b6454d3490cb5a3e4704900fa388ecfe1089f2
Mehdi Masmar <masmar507@gmail.com>
2023-09-27 22:02:24 +0100

    Misunderstood the task lol

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  if (promise) {
-    console.log("Got a response from the API");
-    return {
-      status: 200,
-      body: "success",
-    };
-  }
-  console.log("Got a response from the API");
-  return {
-    error,
-  };
+  return promise
+    .then(() => ({ status: 200, body: "success" }))
+    .catch(() => Error())
+    .finally(() => console.log("Got a response from the API"));
 }
