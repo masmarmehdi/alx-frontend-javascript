c2e981e17b1a5182118f70f3c851ff7c15861275
Mehdi Masmar <masmar507@gmail.com>
2023-09-28 15:17:30 +0100

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return "Evacuate slowly the NUMBER_OF_FLOORS floors";
+  }
+}
