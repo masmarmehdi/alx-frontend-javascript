5873854ae9c49dcd2a921a5649b8268fbc5f50e8
Mehdi Masmar <masmar507@gmail.com>
2023-10-02 09:48:29 +0100

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,6 @@
+export default function getListStudentIds (list_students) {
+    if (!Array.isArray(list_students)) {
+        return [];
+    }
+    return list_students.map((student) => student.id);
+}
