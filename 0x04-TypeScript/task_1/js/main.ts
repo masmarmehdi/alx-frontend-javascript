5473e218767ccb9bf56df6621a57243484af3653
Mehdi Masmar <masmar507@gmail.com>
2023-10-04 22:37:58 +0100

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,30 @@ export const printTeacher: printTeacherFunction = (
   firstName: string,
   lastName: string
 ): string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface StudentClassInterface {
+  _firstName: string;
+  _lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  _firstName: string;
+  _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomeWork(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this._firstName;
+  }
+}
